159037820e8f1e28e9770d4348d21e97d7f3dd4b
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-27 14:58:22 +0530

    Create the card

---
+++ components/Card.tsx
@@ -0,0 +1,24 @@
+
+function Card() {
+    return (
+        <div className="container my-5">
+            
+            <div className="card">
+
+                <div className="card-body">
+
+
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    
+                    <a href="/my-name-is-here" className="btn btn-secondary">Read Now</a>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Card;

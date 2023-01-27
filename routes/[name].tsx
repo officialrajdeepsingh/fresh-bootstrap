802a8942466f8e71ecde00c0b4fd4eeb28f86d5d
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-27 15:00:51 +0530

    Create the reading page

---
+++ routes/[name].tsx
@@ -0,0 +1,39 @@
+import Layout from '../components/Layout.tsx';
+
+export default function Read() {
+  return <Layout>
+
+    <div className="container my-4 pb50">
+
+      <div className="row">
+
+        <div className="col-md-10 m-auto mb-4">
+          <article>
+
+            <img src="https://bootdey.com/img/Content/bg1.jpg" alt="" className="img-fluid mb30" />
+
+            <div className="post-content">
+
+              <h2 className="mt-4">A smart template that works 24/7 for your company</h2>
+              <p className="mb-4">Published By Frontend web</p>
+
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.
+              </p>
+
+              <p className="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.
+              </p>
+
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.
+              </p>
+
+            </div>
+
+          </article>
+        </div>
+
+      </div>
+
+    </div>
+
+  </Layout>
+}

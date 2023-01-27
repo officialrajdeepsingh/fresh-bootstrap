a7b215892c595beb6365f0d8cba8e985e98f1baf
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-27 14:59:15 +0530

    Create the layout for blog

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+import { Head } from "$fresh/runtime.ts";
+import Header from './Header.tsx';
+import Footer from './Footer.tsx';
+
+export default function SharedHead(props) {
+
+  return (
+    <>
+
+    <Head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+
+    </Head>
+    <Header />
+       { props.children } 
+
+    <Footer />
+       
+    </>
+  );
+}
+
